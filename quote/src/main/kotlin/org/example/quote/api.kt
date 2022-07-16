8bbf714c008e9c98556536d7d9ba8f2389e03a36
pagidas <pagakrivos@gmail.com>
2022-07-16 19:59:42 +0100

    Renamed model to api
    Those models are part of the quote api and not internals, thus moved
    them into another file that portrays that.

---
+++ quote/src/main/kotlin/org/example/quote/api.kt
@@ -0,0 +1,46 @@
+package org.example.quote
+
+import org.example.result.Result
+import java.util.Currency
+import java.util.Locale
+
+/**
+ * The driving application interface.
+ */
+fun interface FindQuote: (Int) -> Result<Quote, Problem>
+
+data class Quote(
+    val requestedAmount: Amount,
+    val annualPercentageInterestRate: AnnualPercentageInterestRate,
+    val monthlyRepayment: Repayment,
+    val totalRepayment: Repayment
+)
+
+sealed interface Problem
+data class InvalidLoanAmount(
+    val reason: String = "Loan amount must be within £1000 and £15000 and multiple of £100."
+) : Problem {
+    val status: String = "Bad Request"
+    val code: Int = 400
+}
+data class NotEnoughAvailableLenders(
+    val reason: String = "It is not possible to provide a quote."
+): Problem {
+    val status: String = "Bad Request"
+    val code: Int = 400
+}
+
+data class Repayment(
+    val currency: Currency = Currency.getInstance(Locale.UK),
+    val amount: Double
+)
+data class Amount(
+    val currency: Currency = Currency.getInstance(Locale.UK),
+    val value: Int
+)
+data class AnnualPercentageInterestRate(val value: Double)
+
+typealias Lenders = Collection<Lender>
+data class Lender(val name: String, val annualInterestRate: Double, val available: Int)
+
+
